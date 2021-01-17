2fba50871f2a47b9d82ec6355cc99dcfbdd2198b
Luka Peric <lukaperickv@gmail.com>
2021-01-17 20:38:04 +0100

    commit

---
+++ bolero_finished/bolero/precache-manifest.30f15c297b6d1244855c955c5bb28dff.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135e3fd0ccfbbdd1799cbfca4f187553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f192a570bbdd32df8606",
+    "url": "/static/css/main.2f3f21f4.chunk.css"
+  },
+  {
+    "revision": "3fdd595e9a7bdc5daa27",
+    "url": "/static/js/2.88a51245.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.88a51245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f192a570bbdd32df8606",
+    "url": "/static/js/main.eefb8608.chunk.js"
+  },
+  {
+    "revision": "ee76b5cdfb79e128488d",
+    "url": "/static/js/runtime-main.4db4d166.js"
+  },
+  {
+    "revision": "1d05bd10faab6a982b8c84824639c175",
+    "url": "/static/media/1.1d05bd10.jpg"
+  },
+  {
+    "revision": "31073e0171d0fae58e1b7d557707b6b6",
+    "url": "/static/media/2.31073e01.jpg"
+  },
+  {
+    "revision": "d49f1701050fef89964f559a5d461c4a",
+    "url": "/static/media/3.d49f1701.jpg"
+  },
+  {
+    "revision": "9bf29df958c362c69daf0c85699349fb",
+    "url": "/static/media/4.9bf29df9.jpg"
+  },
+  {
+    "revision": "3214c0060288b4bc982209bc9b46b1ea",
+    "url": "/static/media/boleroMap.3214c006.png"
+  },
+  {
+    "revision": "617d47fa7843769ae6bd69d086ab5037",
+    "url": "/static/media/danceLady.617d47fa.png"
+  },
+  {
+    "revision": "bd5a65d16db2dc9c779fd9c1f0cb4f6b",
+    "url": "/static/media/heroDesktopBg.bd5a65d1.png"
+  },
+  {
+    "revision": "5e86ed4fdc379c3df24201783e11aeb4",
+    "url": "/static/media/hiphop.5e86ed4f.png"
+  },
+  {
+    "revision": "3f6fe70d2baee463f36ea00d4eb217da",
+    "url": "/static/media/instructors_bg.3f6fe70d.png"
+  },
+  {
+    "revision": "e527fcb6703f15855a70869e5a26b5bd",
+    "url": "/static/media/logo.e527fcb6.png"
+  },
+  {
+    "revision": "ea7f1ab262edc01e9fb257fc4b698500",
+    "url": "/static/media/rumba.ea7f1ab2.png"
+  },
+  {
+    "revision": "282dd19b1e125c73b1b1f20b99619eee",
+    "url": "/static/media/salsa.282dd19b.png"
+  },
+  {
+    "revision": "336a7998a69006296728f708c62c656d",
+    "url": "/static/media/vaweHeader.336a7998.png"
+  },
+  {
+    "revision": "5e50f67a5d27feb2d815ec1815413e18",
+    "url": "/static/media/vaweHeaderTall.5e50f67a.png"
+  },
+  {
+    "revision": "9093b690cd552d3b417772d610873bca",
+    "url": "/static/media/zumba.9093b690.png"
+  }
+]);
